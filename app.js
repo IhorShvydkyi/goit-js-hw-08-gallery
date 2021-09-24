498c92f57e711b429770962ae752e6a48877bc46
IhorShvydkyi <igorshvydky35@gmail.com>
2021-09-24 21:36:46 +0300

    last changes

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 galleryContainer.addEventListener('click', onGalleryContainerClick)
 
-console.log(createGalleryCardsMarkup(galleryItems))
 
 function createGalleryCardsMarkup(galleryItems) {
   return galleryItems.map(({preview, original, description}) => {
@@ -93,12 +92,38 @@ function createGalleryCardsMarkup(galleryItems) {
   })
   .join('');
 }
+// open modal
+const modalWindow = document.querySelector('.js-lightbox');
+const modalImage = document.querySelector('.lightbox__image');
 
 function onGalleryContainerClick(evt) {
-  
+  evt.preventDefault();
+  const url = evt.target.dataset.source;
+  modalWindow.classList.add("is-open");
+  modalImage.setAttribute('src', url)
 }
 
+// close modal
+const closeButton = document.querySelector('.lightbox__button');
+closeButton.addEventListener('click', onCloseModalClick);
+function onCloseModalClick() {
+  modalWindow.classList.remove("is-open");
+  modalImage.setAttribute('src', '')
+}
 
+// backdrop modal close
+const backdropModalClose = document.querySelector('.lightbox__overlay')
+backdropModalClose.addEventListener('click', onBackdropClick);
+function onBackdropClick(evt) {
+  if(evt.currentTarget === evt.target) {
+    onCloseModalClick()
+  }
+}
 
-
+// modal close by ESC
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    onCloseModalClick()
+  }
+})
 
